ec3cb250b081f5dc8754c8289a738f1bee53a004
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2020-06-30 23:15:09 +0100

    made ticketStore modular

--- src/Components/App/Store/filterStore.ts
+++ src/Components/App/Store/filterStore.ts
@@ -1,6 +1,7 @@
 import { observable, action, computed } from "mobx";
 import { createContext } from "react";
 import { ITicket } from "../../../Models/ticket";
+import TicketStore from "./ticketStore"
 
 interface filterObject {
   status: string[];
@@ -25,6 +26,9 @@ interface IOption {
 
 class FilterStore {
 
+  private ticketStore = new TicketStore.TicketStore();
+  private tickets = this.ticketStore.tickets;
+
   @observable stati: IStatus[] = [
     {
       id: 1,
@@ -48,80 +52,80 @@ class FilterStore {
     }
   ];
 
-  @observable tickets: ITicket[] = [
-    {
-      author: "Pablo",
-      id: 265,
-      status: "Pending",
-      product: "Product 1",
-      title: "Document does not save",
-      date: "2020-01-01",
-    },
-    {
-      author: "Bujigen",
-      id: 303,
-      status: "Done",
-      product: "Product 2",
-      title: "Crash on startup at Night",
-      date: "2020-02-01",
-    },
-    {
-      author: "Sam",
-      id: 621,
-      status: "Done",
-      product: "Product 1",
-      title: "Require Refund",
-      date: "2020-02-01",
-    },
-    {
-      author: "Pablo",
-      id: 905,
-      status: "Pending",
-      product: "Product 2",
-      title: "Update not instaling",
-      date: "2020-12-01",
-    },
-    {
-      author: "Moj",
-      id: 1005,
-      status: "Low",
-      product: "Product 1",
-      title: "Activiation Key missing",
-      date: "2020-06-01",
-    },
-    {
-      author: "Bujigen",
-      id: 892,
-      status: "Urgent",
-      product: "Product 3",
-      title: "Crash on startup at Night",
-      date: "2020-02-01",
-    },
-    {
-      author: "Sam",
-      id: 222,
-      status: "Done",
-      product: "Product 1",
-      title: "Require Refund",
-      date: "2020-02-01",
-    },
-    {
-      author: "Pablo",
-      id: 10,
-      status: "Pending",
-      product: "Product 2",
-      title: "Update not instaling",
-      date: "2020-12-01",
-    },
-    {
-      author: "Moj",
-      id: 45,
-      status: "Low",
-      product: "Product 1",
-      title: "Activiation Key missing",
-      date: "2020-06-01",
-    },
-  ];
+  // @observable tickets: ITicket[] = [
+  //   {
+  //     author: "Pablo",
+  //     id: 265,
+  //     status: "Pending",
+  //     product: "Product 1",
+  //     title: "Document does not save",
+  //     date: "2020-01-01",
+  //   },
+  //   {
+  //     author: "Bujigen",
+  //     id: 303,
+  //     status: "Done",
+  //     product: "Product 2",
+  //     title: "Crash on startup at Night",
+  //     date: "2020-02-01",
+  //   },
+  //   {
+  //     author: "Sam",
+  //     id: 621,
+  //     status: "Done",
+  //     product: "Product 1",
+  //     title: "Require Refund",
+  //     date: "2020-02-01",
+  //   },
+  //   {
+  //     author: "Pablo",
+  //     id: 905,
+  //     status: "Pending",
+  //     product: "Product 2",
+  //     title: "Update not instaling",
+  //     date: "2020-12-01",
+  //   },
+  //   {
+  //     author: "Moj",
+  //     id: 1005,
+  //     status: "Low",
+  //     product: "Product 1",
+  //     title: "Activiation Key missing",
+  //     date: "2020-06-01",
+  //   },
+  //   {
+  //     author: "Bujigen",
+  //     id: 892,
+  //     status: "Urgent",
+  //     product: "Product 3",
+  //     title: "Crash on startup at Night",
+  //     date: "2020-02-01",
+  //   },
+  //   {
+  //     author: "Sam",
+  //     id: 222,
+  //     status: "Done",
+  //     product: "Product 1",
+  //     title: "Require Refund",
+  //     date: "2020-02-01",
+  //   },
+  //   {
+  //     author: "Pablo",
+  //     id: 10,
+  //     status: "Pending",
+  //     product: "Product 2",
+  //     title: "Update not instaling",
+  //     date: "2020-12-01",
+  //   },
+  //   {
+  //     author: "Moj",
+  //     id: 45,
+  //     status: "Low",
+  //     product: "Product 1",
+  //     title: "Activiation Key missing",
+  //     date: "2020-06-01",
+  //   },
+  // ];
 
   @observable filteredTickets: ITicket[] = [...this.tickets];
 
