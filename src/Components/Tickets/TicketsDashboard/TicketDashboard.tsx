a3908d0856b54a99356f20fa81a1d9fb817dae09
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2020-06-30 15:38:44 +0100

    added-view-button

--- src/Components/Tickets/TicketsDashboard/TicketDashboard.tsx
+++ src/Components/Tickets/TicketsDashboard/TicketDashboard.tsx
@@ -10,11 +10,11 @@ const TicketDashboard = () => {
             <hr/>
             <Grid columns={6} id="ticketsHeader">
                 <Grid.Column width={2} className="remove-padding table-header">Author</Grid.Column>
-                <Grid.Column width={1} className="remove-padding table-header">Id</Grid.Column>
                 <Grid.Column width={3} className="remove-padding table-header">Status</Grid.Column>
-                <Grid.Column width={3} className="remove-padding table-header">Product</Grid.Column>
+                <Grid.Column width={2} className="remove-padding table-header">Product</Grid.Column>
                 <Grid.Column width={4} className="remove-padding table-header">Title</Grid.Column>
                 <Grid.Column width={3} className="remove-padding table-header">Date</Grid.Column>
+                <Grid.Column width={2} className="remove-padding table-header"></Grid.Column>
             </Grid>
             <hr/>
             <TicketList/>
