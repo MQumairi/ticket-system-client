2742640a3bce4c72d7f965f825d085eecbdcbebb
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2020-07-01 00:54:12 +0100

    ticket creation mobx bug with TS computed readonly

--- src/Components/App/Store/filterStore.ts
+++ src/Components/App/Store/filterStore.ts
@@ -1,7 +1,7 @@
-import { observable, action, computed } from "mobx";
+import { observable, action, computed, decorate } from "mobx";
 import { createContext } from "react";
 import { ITicket } from "../../../Models/ticket";
-import TicketStore from "./ticketStore"
+import TicketStore from "./ticketStore";
 
 interface filterObject {
   status: string[];
@@ -19,13 +19,12 @@ interface IStatus {
 }
 
 interface IOption {
-  key: number,
-  text: string,
-  value: string
+  key: number;
+  text: string;
+  value: string;
 }
 
 class FilterStore {
-
   private ticketStore = new TicketStore.TicketStore();
   private tickets = this.ticketStore.tickets;
 
@@ -49,83 +48,8 @@ class FilterStore {
       id: 4,
       name: "Done",
       color: "#45B510",
-    }
+    },
   ];
-
-  // @observable tickets: ITicket[] = [
-  //   {
-  //     author: "Pablo",
-  //     id: 265,
-  //     status: "Pending",
-  //     product: "Product 1",
-  //     title: "Document does not save",
-  //     date: "2020-01-01",
-  //   },
-  //   {
-  //     author: "Bujigen",
-  //     id: 303,
-  //     status: "Done",
-  //     product: "Product 2",
-  //     title: "Crash on startup at Night",
-  //     date: "2020-02-01",
-  //   },
-  //   {
-  //     author: "Sam",
-  //     id: 621,
-  //     status: "Done",
-  //     product: "Product 1",
-  //     title: "Require Refund",
-  //     date: "2020-02-01",
-  //   },
-  //   {
-  //     author: "Pablo",
-  //     id: 905,
-  //     status: "Pending",
-  //     product: "Product 2",
-  //     title: "Update not instaling",
-  //     date: "2020-12-01",
-  //   },
-  //   {
-  //     author: "Moj",
-  //     id: 1005,
-  //     status: "Low",
-  //     product: "Product 1",
-  //     title: "Activiation Key missing",
-  //     date: "2020-06-01",
-  //   },
-  //   {
-  //     author: "Bujigen",
-  //     id: 892,
-  //     status: "Urgent",
-  //     product: "Product 3",
-  //     title: "Crash on startup at Night",
-  //     date: "2020-02-01",
-  //   },
-  //   {
-  //     author: "Sam",
-  //     id: 222,
-  //     status: "Done",
-  //     product: "Product 1",
-  //     title: "Require Refund",
-  //     date: "2020-02-01",
-  //   },
-  //   {
-  //     author: "Pablo",
-  //     id: 10,
-  //     status: "Pending",
-  //     product: "Product 2",
-  //     title: "Update not instaling",
-  //     date: "2020-12-01",
-  //   },
-  //   {
-  //     author: "Moj",
-  //     id: 45,
-  //     status: "Low",
-  //     product: "Product 1",
-  //     title: "Activiation Key missing",
-  //     date: "2020-06-01",
-  //   },
-  // ];
 
   @observable filteredTickets: ITicket[] = [...this.tickets];
 
@@ -146,9 +70,9 @@ class FilterStore {
       returnArr.push({
         key: status.id,
         text: status.name,
-        value: status.name
-      })
-    })
+        value: status.name,
+      });
+    });
 
     return returnArr;
   }
